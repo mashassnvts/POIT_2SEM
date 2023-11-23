71572427b4cc98ef57291c650cc06e13a57833f7
Maria <119621261+mashassnvts@users.noreply.github.com>
2023-11-23 23:01:00 +0300

    Add files via upload

---
+++ SE/лаба 4/SE_Lab03/SE_Lab03/check_format01.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "stdafx.h"
+
+bool check_format01(string date, bool tip);
+
---
+++ SE/лаба 4/SE_Lab04/task_2/task_2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+//дисциплина: название, курс (на котором читается),
+//семестр / семестры, количество_лекций_в_семестре
+
+using namespace std;
+
+struct Disipline
+{
+    string nazv;
+    short course;
+    short sem;
+    int lectures;
+};
+
+bool operator ==(Disipline d1, Disipline d2)
+{
+    return (d1.sem == d2.sem);
+}
+
+bool operator >(Disipline d1, Disipline d2)
+{
+    return (d1.lectures > d2.lectures);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+
+    Disipline d1 = { "kpo",3,2, 36 };
+    Disipline d2 = { "oap", 1,2, 72 };
+    if (d1 == d2)
+    {
+        cout << "d1 and d2 равны,тк одинаковое кол-во семов\n";
+    }
+    else
+    {
+        cout << "d1 и d2 имеют разное кол-во семов\n";
+    }
+    if (d1 > d2)
+    {
+        cout << "d1 and d2 равны,тк одинаковое часов лекций\n";
+    }
+    else
+    {
+        cout << "d1 и d2 имеют разное часов лекций\n";
+    }
+
+    return 0;
+}
