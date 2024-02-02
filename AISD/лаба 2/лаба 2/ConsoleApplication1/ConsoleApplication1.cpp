397fae1680258ea1e108db1d04ea73d4a3fe3c13
Maria <mariasosnovec@gmail.com>
2024-02-02 22:41:32 +0300

    aisd discipline added

---
+++ AISD/лаба 2/лаба 2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int N, choice = 0, left = 1, right, mid;
+	int right2, left2 = left;
+	bool check = false;
+	cout << "Введите диапозон чисел (от 1 до ?) "; cin >> right;
+	right2 = right;
+	cout << "Введите загаданное число :) ";	cin >> N;
+	mid = right / 2;
+	do {
+		if (choice == 1) {
+			left = mid;
+			mid = (right + left) / 2;
+		}
+		if (choice == 2) {
+			right = mid;
+			mid = (right + left) / 2;
+		}
+		if (choice == 3 || mid == N)
+			check = true;
+		system("cls");
+		cout << mid << endl;
+		cout << "1)Больше\n2)Меньше\n3)Угадал" << endl;
+		cin >> choice;
+	} while (check != true);
+	system("cls");
+	cout << "Угаданное число: " << N << endl;
+	cout << "-------------------------\n";
+	cout << "Максимальное количество шагов = " << round(log2(right2)) << endl;
+
+	for (int m = 1, n = right2; n > 1; m++)
+	{
+		mid = (left2 + right2) / 2;
+		cout << "Шаг " << m << " число " << mid << endl;
+		right2 = mid;
+		n = mid;
+	}
+	return 0;
+}
---
+++ AISD/лаба 2/лаба 2/лаба 2/лаба 2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "rus"); 
+
+    int n, guess, min_num = 1, max_num, count = 0, max_count = 0;
+    
+    vector<int> guesses;
+
+    cout << "Введите диапазон: ";
+    cin >> n;
+
+    max_num = n;
+    
+    while (true) 
+    {
+        guess = (min_num + max_num) / 2;
+        count++;
+
+        guesses.push_back(guess);
+
+        cout << "Я думаю, что это число " << guess << ". Выберите вариант ответа:" << endl;
+        cout << "1. Много" << endl;
+        cout << "2. Мало" << endl;
+        cout << "3. Угадал" << endl;
+
+
+        int response;
+        cin >> response;
+
+        switch (response) 
+        {
+        case 1:
+
+            max_num = guess - 1;
+            break;
+
+        case 2:
+
+            min_num = guess + 1;
+            break;
+
+        case 3:
+
+            if (count > max_count) 
+            {
+                max_count = count;
+            }
+
+            cout << "Максимальное количество шагов = " << max_count << endl;
+            cout << "История предположений: ";
+
+            for (int i = 0; i < guesses.size(); i++) 
+            {
+                cout << guesses[i] << " ";
+            }
+
+            cout << endl;
+            return 0;
+
+        default:
+            cout << "Неправильный ввод. Попробуйте еще раз." << endl;
+            break;
+        }
+    }
+}
