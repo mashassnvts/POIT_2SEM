f10ae3f59a5b124831d3817195144a2ce4da9fd1
Maria <119621261+mashassnvts@users.noreply.github.com>
2024-02-02 23:11:00 +0300

    Add files via upload

---
+++ SE/лаба 4/SE_Lab03/SE_Lab03/find_substr.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "stdafx.h"
+int* find_substr(string date, int datearr[]);
+
---
+++ SE/лаба 4/SE_Lab03/SE_Lab03/get_birthday_date.h
@@ -0,0 +1,3 @@
+#include "stdafx.h"
+int get_birthday(int  monthnow, int daynow, int monthbirth, int daybirth, int yearbirth, char yr[]);
+#pragma once
